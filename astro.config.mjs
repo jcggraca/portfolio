d8c564392448a215def0ceb8f9dad4e71186e181
João Graça <jgraca95@gmail.com>
2025-02-11 15:11:08 +0000

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  experimental: {
+    svg: true,
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  site: "https://joaograca.dev",
+});

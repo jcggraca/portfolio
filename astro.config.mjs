4d1822b7b0f821e9608243263fd6969e9b5ed051
João Graça <jgraca95@gmail.com>
2025-03-07 21:06:58 +0000

    Improve SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,18 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     svg: true,
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   site: "https://joaograca.dev",
+  integrations: [sitemap()],
 });
